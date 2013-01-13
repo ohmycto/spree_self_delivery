bb5bcc73655e6d991fd0abf3bfd0eaf02146dacc
Babur Usenakunov <bob.usenakunov@gmail.com>
2013-01-14 00:35:08 +0600

    Self delivery points in configurations menu

--- app/overrides/views_decorator.rb
+++ app/overrides/views_decorator.rb
@@ -1,7 +1,12 @@
+Deface::Override.new(:virtual_path => "admin/configurations/index",
+                     :insert_bottom => "[data-hook='configuration']",
+                     :partial => "admin/shared/configurations_self_delivery_points",
+                     :name => "configurations_self_delivery_points")
+
 Deface::Override.new(:virtual_path => "admin/shared/_configuration_menu",
                      :insert_bottom => "[data-hook='admin_configurations_sidebar_menu']",
-                     :partial => "admin/shared/configuration_menu_pickup_points",
-                     :name => "configuration_menu_pickup_points")
+                     :partial => "admin/shared/configurations_menu_self_delivery_points",
+                     :name => "configurations_menu_self_delivery_points")
 
 Deface::Override.new(:virtual_path => "checkout/_address",
                      :replace => "hr.space",
