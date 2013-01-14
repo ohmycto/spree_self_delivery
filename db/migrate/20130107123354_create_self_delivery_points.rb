dc6bbcb22862cdf6a6c649b540da08a697abb8c6
Babur Usenakunov <bob.usenakunov@gmail.com>
2013-01-14 22:45:26 +0600

    Merge migrations

--- db/migrate/20130107123354_create_self_delivery_points.rb
+++ db/migrate/20130107123354_create_self_delivery_points.rb
@@ -3,10 +3,14 @@ class CreateSelfDeliveryPoints < ActiveRecord::Migration
     create_table :self_delivery_points do |t|
       t.integer :country_id
       t.integer :state_id
+      t.string :state_name
       t.string :city
       t.string :address1
       t.string :hours
       t.text :description
+      t.boolean :show_country
+      t.boolean :show_state
+      t.decimal :cost, :precision => 8, :scale => 2
 
       t.timestamps
     end
--- db/migrate/20130113164339_add_state_name_to_self_delivery_points.rb
+++
@@ -1,5 +0,0 @@
-class AddStateNameToSelfDeliveryPoints < ActiveRecord::Migration
-  def change
-    add_column :self_delivery_points, :state_name, :string
-  end
-end
